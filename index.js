0e74dbff02231818cd31195b628cb89543d3b0fd
joe welch <jwelch19@outlook.com>
2022-03-08 20:51:35 -0500

    commit tree 53266fcfa56e66a5f17a879ca39ba5b69e934a90

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(num1){
+    if (num1 >= 42) {
+        return num1 - 42;
+    }
+   
+ else if (num1 < 42) {
+     return 42 - num1;
+ }
+}
+
+distanceFromHqInBlocks(43);
+
+function distanceFromHqInFeet(num1){
+    return distanceFromHqInBlocks(num1) * 264;
+}
+
+distanceFromHqInFeet(num1);
+
+function distanceTravelledInFeet(start, destination){
+    if (start >= destination) {
+        return (start - destination) * 264;
+    }
+
+   else if (start < destination) {
+        return (destination - start) * 264;
+    }
+}
+distanceTravelledInFeet(start, destination);
+
+function calculatesFarePrice(start, destination) {
+    if (distanceTravelledInFeet(start, destination) < 400) {
+        return 0
+    }
+
+    else if (distanceTravelledInFeet(start, destination) >= 400 && distanceTravelledInFeet(start, destination) < 2000) {
+        return (distanceTravelledInFeet(start, destination) - 400) * .02;
+    }
+
+    else if (distanceTravelledInFeet(start, destination) >= 2000 && distanceTravelledInFeet(start, destination) < 2500) {
+        return 25;
+    }
+
+    else if (distanceTravelledInFeet(start, destination) > 2500) {
+        return "cannot travel that far";
+        }
+    }
+calculatesFarePrice(start, destination);
